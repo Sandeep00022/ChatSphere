db9687f00695bffd6968c175a803b83f83ee318c
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-01-24 17:21:51 +0530

    feat: Initialize project with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const chats = require("./data/data");
+const env = require("dotenv");
+env.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+  console.log(singleChat);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(8080, () => {
+  console.log(`your port is running at ${port}`);
+});
